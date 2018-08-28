2ec1e7fd9d37b0cd5aaa6a194c7520994552ce77
nebed <nebeduch@gmail.com>
2018-08-28 09:11:46 +0100

    CleanUp 1

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -9,86 +9,7 @@
     ================================================== -->
     <div class="s-pageheader">
 
-        <header class="header">
-            <div class="header__content row">
-
-                <div class="header__logo">
-                    <a class="logo" href="index.html">
-                        <img src="{{ URL::asset('images/logo.svg') }}" alt="Homepage">
-                    </a>
-                </div> <!-- end header__logo -->
-
-                <ul class="header__social">
-                    <li>
-                        <a href="#0"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                    </li>
-                    <li>
-                        <a href="#0"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                    </li>
-                    <li>
-                        <a href="#0"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                    </li>
-                    <li>
-                        <a href="#0"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-                    </li>
-                </ul> <!-- end header__social -->
-
-                <a class="header__search-trigger" href="#0"></a>
-
-                <div class="header__search">
-
-                    <form role="search" method="get" class="header__search-form" action="#">
-                        <label>
-                            <span class="hide-content">Search for:</span>
-                            <input type="search" class="search-field" placeholder="Type Keywords" value="" name="s" title="Search for:" autocomplete="off">
-                        </label>
-                        <input type="submit" class="search-submit" value="Search">
-                    </form>
-        
-                    <a href="#0" title="Close Search" class="header__overlay-close">Close</a>
-
-                </div>  <!-- end header__search -->
-
-
-                <a class="header__toggle-menu" href="#0" title="Menu"><span>Menu</span></a>
-
-                <nav class="header__nav-wrap">
-
-                    <h2 class="header__nav-heading h6">Site Navigation</h2>
-
-                    <ul class="header__nav">
-                        <li><a href="index.html" title="">Home</a></li>
-                        <li class="has-children">
-                            <a href="#0" title="">Categories</a>
-                            <ul class="sub-menu">
-                            <li><a href="category.html">Lifestyle</a></li>
-                            <li><a href="category.html">Health</a></li>
-                            <li><a href="category.html">Family</a></li>
-                            <li><a href="category.html">Management</a></li>
-                            <li><a href="category.html">Travel</a></li>
-                            <li><a href="category.html">Work</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-children current">
-                            <a href="#0" title="">Blog</a>
-                            <ul class="sub-menu">
-                            <li><a href="single-video.html">Video Post</a></li>
-                            <li><a href="single-audio.html">Audio Post</a></li>
-                            <li><a href="single-gallery.html">Gallery Post</a></li>
-                            <li><a href="single-standard.html">Standard Post</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="style-guide.html" title="">Styles</a></li>
-                        <li><a href="about.html" title="">About</a></li>
-                        <li><a href="contact.html" title="">Contact</a></li>
-                    </ul> <!-- end header__nav -->
-
-                    <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu">Close</a>
-
-                </nav> <!-- end header__nav-wrap -->
-
-            </div> <!-- header-content -->
-        </header> <!-- header -->
+         @include('partials._nav')
 
     </div> <!-- end s-pageheader -->
 
@@ -101,6 +22,8 @@
 
             <div class="s-content__header col-full">
 
+                @include('partials._messages')
+
                 <h1 class="s-content__header-title">
                     {{$post->title}}
                 </h1>
@@ -108,18 +31,18 @@
                     <li class="date">{{date('F j, Y g:ia',strtotime($post->created_at))}}</li>
                     <li class="cat">
                         In
-                        <a href="#0">Lifestyle</a>
-                        <a href="#0">Travel</a>
+                        <a href="#0">{{$post->category->name}}</a>
                     </li>
                 </ul>
             </div> <!-- end s-content__header -->
-    
+             @if(!empty($post->featured_image))
             <div class="s-content__media col-full">
                 <div class="s-content__post-thumb">
                     <img src="{{ asset('images/'.$post->featured_image) }}" 
                          sizes="(max-width: 2000px) 100vw, 2000px" alt="" >
                 </div>
             </div> <!-- end s-content__media -->
+            @endif
 
             <div class="col-full s-content__main">
                 {!!$post->body!!}
@@ -138,7 +61,7 @@
 
                     <div class="s-content__author-about">
                         <h4 class="s-content__author-name">
-                            <a href="#0">Jonathan Doe</a>
+                            <a href="#0">Betty Kings</a>
                         </h4>
                     
                         <p>Alias aperiam at debitis deserunt dignissimos dolorem doloribus, fuga fugiat impedit laudantium magni maxime nihil nisi quidem quisquam sed ullam voluptas voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -213,119 +136,6 @@
 
                         </li> <!-- end comment level 1 -->
                     @endforeach
-
-                        <li class="thread-alt depth-1 comment">
-
-                            <div class="comment__avatar">
-                                <img width="50" height="50" class="avatar" src="images/avatars/user-04.jpg" alt="">
-                            </div>
-
-                            <div class="comment__content">
-
-                                <div class="comment__info">
-                                <cite>John Doe</cite>
-
-                                <div class="comment__meta">
-                                    <time class="comment__time">Dec 16, 2017 @ 24:05</time>
-                                    <a class="reply" href="#0">Reply</a>
-                                </div>
-                                </div>
-
-                                <div class="comment__text">
-                                <p>Sumo euismod dissentiunt ne sit, ad eos iudico qualisque adversarium, tota falli et mei. Esse euismod
-                                urbanitas ut sed, et duo scaevola pericula splendide. Primis veritus contentiones nec ad, nec et
-                                tantas semper delicatissimi.</p>
-                                </div>
-
-                            </div>
-
-                            <ul class="children">
-
-                                <li class="depth-2 comment">
-
-                                    <div class="comment__avatar">
-                                        <img width="50" height="50" class="avatar" src="images/avatars/user-03.jpg" alt="">
-                                    </div>
-
-                                    <div class="comment__content">
-
-                                        <div class="comment__info">
-                                            <cite>Kakashi Hatake</cite>
-
-                                            <div class="comment__meta">
-                                                <time class="comment__time">Dec 16, 2017 @ 25:05</time>
-                                                <a class="reply" href="#0">Reply</a>
-                                            </div>
-                                        </div>
-
-                                        <div class="comment__text">
-                                            <p>Duis sed odio sit amet nibh vulputate
-                                            cursus a sit amet mauris. Morbi accumsan ipsum velit. Duis sed odio sit amet nibh vulputate
-                                            cursus a sit amet mauris</p>
-                                        </div>
-
-                                    </div>
-
-                                    <ul class="children">
-
-                                        <li class="depth-3 comment">
-
-                                            <div class="comment__avatar">
-                                                <img width="50" height="50" class="avatar" src="images/avatars/user-04.jpg" alt="">
-                                            </div>
-
-                                            <div class="comment__content">
-
-                                                <div class="comment__info">
-                                                <cite>John Doe</cite>
-
-                                                <div class="comment__meta">
-                                                    <time class="comment__time">Dec 16, 2017 @ 25:15</time>
-                                                    <a class="reply" href="#0">Reply</a>
-                                                </div>
-                                                </div>
-
-                                                <div class="comment__text">
-                                                <p>Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est
-                                                etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
-                                                </div>
-
-                                            </div>
-
-                                        </li>
-
-                                    </ul>
-
-                                </li>
-
-                            </ul>
-
-                        </li> <!-- end comment level 1 -->
-
-                        <li class="depth-1 comment">
-
-                            <div class="comment__avatar">
-                                <img width="50" height="50" class="avatar" src="images/avatars/user-02.jpg" alt="">
-                            </div>
-
-                            <div class="comment__content">
-
-                                <div class="comment__info">
-                                <cite>Shikamaru Nara</cite>
-
-                                <div class="comment__meta">
-                                    <time class="comment-time">Dec 16, 2017 @ 25:15</time>
-                                    <a class="reply" href="#">Reply</a>
-                                </div>
-                                </div>
-
-                                <div class="comment__text">
-                                <p>Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem.</p>
-                                </div>
-
-                            </div>
-
-                        </li>  <!-- end comment level 1 -->
 
                     </ol> <!-- end commentlist -->
 
