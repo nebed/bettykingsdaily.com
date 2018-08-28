f20c6e3c849ec0400f74f7d5c26e112be263d8c0
nebed <nebeduch@gmail.com>
2018-08-28 11:47:51 +0100

    Created Categories View

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,28 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\Tag;
+use App\Category;
 use App\Http\Requests;
 
 class BlogController extends Controller
 {
 
 	public function getIndex(){
+        $categorylist = Category::all();
 		$posts = Post::orderBy('created_at', 'desc')->paginate(12);
         $tagfoot = Tag::all();
-		return view('blog.index')->withPosts($posts)->withTagfoot($tagfoot);
+		return view('blog.index')->withPosts($posts)->withTagfoot($tagfoot)->withCategorylist($categorylist);
 	}
 
     public function getSingle($slug){
+        $categorylist = Category::all();
     	//fetch post with slug from db
         $tagfoot = Tag::all();
     	$post= Post::where('slug', '=', $slug)->first(); //first is like limit 1
 
     	//return the view and pass in the post 
-    	return view('blog.single')->withPost($post)->withTagfoot($tagfoot);
+    	return view('blog.single')->withPost($post)->withTagfoot($tagfoot)->withCategorylist($categorylist);
     }
 
     public function getList($name){
-        
+        $categorylist = Category::all();
+        $tagfoot = Tag::all();
+        $category_id = Category::where('name', '=', $name)->first()->id;
+        $posts =Post::whereHas("category", function($q) use ($category_id){
+   $q->where('id','=',$category_id);
+})->paginate(12);
+        return view('blog.index')->withPosts($posts)->withTagfoot($tagfoot)->withCategorylist($categorylist);
+
     }
 
 }
